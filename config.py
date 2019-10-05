793b4ae4124eea91941b37049cbd8ab200abf0a5
jinzaizhichi <jinzaizhichi9888@gmail.com>
2019-10-05 19:14:06 +0800

    更正

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ KindleEar配置文件，请务必修改开始两个配置（如果使用uploader
 """
 
 SRC_EMAIL = "jinzaizhichi9888@gmail.com"  #Your gmail account for sending mail to Kindle
-DOMAIN = "https://kd1.jinzaizhichi8.com" #Your domain of app
+DOMAIN = "https://kd.jinzaizhichi8.com" #Your domain of app
 
 TIMEZONE = 8  #Default timezone, you can modify it in webpage after deployed
 
